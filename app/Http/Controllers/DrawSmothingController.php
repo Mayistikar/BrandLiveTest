8843829323e13353830ee20aba7bdb993ce0b40a
arodriguezce <anderson.rodriguez@avista.co>
2021-04-14 02:30:49 -0500

    Adding permutations

--- app/Http/Controllers/DrawSmothingController.php
+++ app/Http/Controllers/DrawSmothingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class DrawSmothingController extends Controller
 {
@@ -13,10 +14,15 @@ class DrawSmothingController extends Controller
         $chars = $request->letters;
         $size = $request->charNumber;
         $just_words = $request->justWords;
-        echo $just_words;
-        echo json_encode($request->all());        
+        // echo $just_words;
+        // echo json_encode($request->all());        
         $this->getCombinations($chars, $size);
-        $combinations = $this->combs;        
+        $combinations = $this->combs;
+        // $combinations = $this->getPermutations($chars, $size);
+        // echo json_encode($combinations);
+
+        $this->getRealWords(implode(" ",$this->combs));
+
         return view('draw-smothing', compact('combinations'));
     }
 
@@ -39,5 +45,63 @@ class DrawSmothingController extends Controller
             $data[$index] = $arr[$i];
             $this->combinationUtil($arr, $data, $i + 1, $end, $index + 1, $word_size);
         }
+    }
+
+
+    function getPermutations($characters, $word_size)
+    {
+        $characters_array = str_split($characters);        
+        $characters_size = sizeof($characters_array);        
+        return $this->permutationUtil($characters_size, $characters_array);
+    }
+
+    function permutationUtil($characters_size, $characters_array)
+    {
+        if ($characters_size == 1)
+        {
+            echo json_encode($characters_array);
+            return $characters_array;
+        }
+        $this->permutationUtil($characters_size - 1, $characters_array);
+        for ($i = 0; $i < $characters_size; $i++)
+        {
+            if ($characters_size % 2 == 0)
+            {
+                $temp = $characters_array[$i];
+                $characters_array[$i] = $characters_array[$characters_size-1];
+                $characters_array[$characters_size-1] = $temp;                
+            }                
+            else
+            {
+                $temp = $characters_array[0];
+                $characters_array[0] = $characters_array[$characters_size-1];
+                $characters_array[$characters_size-1] = $temp;   
+            }                
+        }
+
+    }
+
+    private function getRealWords($words)
+    {
+        $HTTPresponse = Http::post('https://language.googleapis.com/v1/documents:analyzeSyntax?key=AIzaSyCK4Qe6SkvgTrGfMjDDq2v7cdoDWMOApVc', [
+            'encodingType' => 'UTF8',
+            'document' => [
+                "type" => "PLAIN_TEXT",
+                "language" => "es",        
+                "content" => $words
+            ],
+        ]);
+        $response = json_decode($HTTPresponse,true);
+        $real_words = array();
+        foreach ($response["tokens"] as &$token) {
+            // echo $token["text"]["content"];
+            // echo $token["partOfSpeech"]["aspect"];
+            // echo "\n";
+            if (strcmp($token["partOfSpeech"]["aspect"], "ASPECT_UNKNOWN") <> 0 ) {
+                array_push($real_words, $token["text"]["content"]);
+            }
+        }
+        echo json_encode($real_words);
+        return $real_words;
     }
 }
